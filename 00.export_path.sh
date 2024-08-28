9587741307e959cbf7b9896a86f40bd91719dd62
kimzz14 <kimzz1004@gmail.com>
2024-08-28 11:15:39 +0900

    init

---
+++ 00.export_path.sh
@@ -0,0 +1,15 @@
+##################################################################################
+
+source activate genomescope2_v2.0_240729
+
+##################################################################################
+#conda create -n genomescope2_v2.0_240729
+#
+#source activate genomescope2_v2.0_240729
+#
+#conda install genomescope2
+#bioconda/noarch::genomescope2-2.0-py312r43hdfd78af_7
+#
+#conda install kmc
+#bioconda/linux-64::kmc-3.2.4-h6dccd9a_1
+##################################################################################
