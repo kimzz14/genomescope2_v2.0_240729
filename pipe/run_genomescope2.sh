0e12327b47b8ad07bace194fcec19e5878930899
kimzz14 <kimzz1004@gmail.com>
2024-09-04 13:43:28 +0900

    bug fix

--- pipe/run_genomescope2.sh
+++ pipe/run_genomescope2.sh
@@ -6,6 +6,10 @@ if [ -z "${kmer}" ]; then
     exit 1
 fi
 
-kmc -k${kmer} -t${threadN} -m64 -ci1 -cs1000000000 -fq @FILES k${kmer} tmp 1> k${kmer}.log 2>&1
+if [ ! -d "tmp_k${kmer}" ]; then
+    mkdir "tmp_k${kmer}"
+fi
+
+kmc -k${kmer} -t${threadN} -m64 -ci1 -cs1000000000 -fq @FILES k${kmer} tmp_k${kmer} 1> k${kmer}.log 2>&1
 kmc_tools transform k${kmer} histogram k${kmer}.histo -cx100000 1> k${kmer}.histo.log 2>&1
 genomescope2 -i k${kmer}.histo -o k${kmer} -k ${kmer} -p 2 1> k${kmer}.genomescope2.log 2>&1
